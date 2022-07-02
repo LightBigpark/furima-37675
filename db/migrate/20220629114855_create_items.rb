a3d7c6e31e74d723b8d2ef8595486660b43e575c
LightBigpark <hikarioozono@gmail.com>
2022-07-02 14:42:11 +0900

    マイグレーションファイルを削除、コメントアウトしました

--- db/migrate/20220629114855_create_items.rb
+++ db/migrate/20220629114855_create_items.rb
@@ -1,18 +1,18 @@
-class CreateItems < ActiveRecord::Migration[6.0]
-  def change
-    create_table :items do |t|
-      t.text       :image,        null: false
-      t.string     :items_name,   null: false
-      t.text       :explain,      null: false
-      t.integer    :category_id,  null: false
-      t.integer    :condition_id, null: false
-      t.integer    :delivery_id,  null: false
-      t.integer    :area_id,      null: false
-      t.integer    :date_id,      null: false
-      t.integer    :price,        null: false
-      t.references :users,        null: false, foreign_key: true
+#class CreateItems < ActiveRecord::Migration[6.0]
+  #def change
+    #create_table :items do |t|
+      #t.text       :image,        null: false
+      #t.string     :items_name,   null: false
+      #t.text       :explain,      null: false
+      #t.integer    :category_id,  null: false
+      #t.integer    :condition_id, null: false
+      #t.integer    :delivery_id,  null: false
+      #t.integer    :area_id,      null: false
+      #t.integer    :date_id,      null: false
+      #t.integer    :price,        null: false
+      #t.references :users,        null: false, foreign_key: true
 
-      t.timestamps
+      #t.timestamps
     end
   end
 end
