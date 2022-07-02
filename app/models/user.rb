cc11d2dfb925df3e63645b339403f044f5ea7c3c
LightBigpark <hikarioozono@gmail.com>
2022-07-02 17:28:50 +0900

    userモデルのバリデーションを記述しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
-end
+         validates :nickname,           presence: true
+
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
+         validates :last_name
+         validates :first_name
+         end
+         with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: '全角カタカナを使用してください' }do
+         validates :last_kana
+         validates :first_kana
+        end
+
+         validates :birthday,           presence: true
+
+         validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください" }
+       end
+  end
