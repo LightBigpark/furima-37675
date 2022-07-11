2d91b35eb1814a62c732656462adb182b5f680be
LightBigpark <hikarioozono@gmail.com>
2022-07-11 16:21:57 +0900

    ストロングパラメーター

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,9 @@ class ItemsController < ApplicationController
   def index
   end
 
+  private
+
+   def message_params
+    params.require(:items).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
