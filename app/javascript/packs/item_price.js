6d9bbb28044869fc2f0589d423c4912cf2123f13
LightBigpark <hikarioozono@gmail.com>
2022-07-12 17:33:01 +0900

    価格の表示の部分を記述しました

--- app/javascript/packs/item_price.js
+++ app/javascript/packs/item_price.js
@@ -1,7 +1,14 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener('input', () => {
-  const inputValue = priceInput.value;
-  console.log(inputValue);
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.round(inputValue * 0.1)
+    const profitNumber = document.getElementById("profit");
+    profitNumber.innerHTML = (Math.floor(inputValue - addTaxDom));
+    const value_result = inputValue * 0.1
+    console.log(value_result)
+    profitNumber.innerHTML = (Math.floor(inputValue - value_result));
+      console.log(profitNumber)
   })
 })
