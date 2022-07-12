e1b8261ce1d2e3bc66aa92b9028e5954f37132e7
LightBigpark <hikarioozono@gmail.com>
2022-07-12 15:02:34 +0900

    アクションを追加しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,19 @@ class ItemsController < ApplicationController
 
   def new
       before_action :authenticate_user!
+      @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
   end
 
   private
+
+  def item_params
+    params.require(:items).permit(:image, :items_name,:explain,:category_id,:condition_id,:delivery_id,:area_id,:date_id,:price,:users)
+  end
+
 
    def message_params
     params.require(:items).permit(:content, :image).merge(user_id: current_user.id)
