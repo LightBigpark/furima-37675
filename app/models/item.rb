7cd50d380e20f0ab6121805e50213aedcd08f3ab
LightBigpark <hikarioozono@gmail.com>
2022-07-13 18:36:21 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,24 +2,26 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
-  belongs_to :area
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :date
-  belongs_to :delivery
+  has_many :area
+  has_many :category
+  has_many :condition
+  has_many :date
+  has_many :delivery
 
-  with_options presence: true
+  with_options presence: true do
     validates :image
     validates :items_name
     validates :explain
   end
 
-  with_options numericality: { other_than: 1 , message: "can't be blank"}
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
   validates :category_id
   validates :condition_id
   validates :delivery_id
   validates :area_id
   validates :date_id
+  end
 
 
-  validates :price,      presence: true, numericality: { in: 300..9999999 }, format: { with: /^[0-90-9]+$/}
+  validates :price,      presence: true, numericality: { in: 300..9999999 }, format: { with: /^[0-9]+$/}
+end
