3446e8b2087806357c72ca6bb947c6be2461070b
LightBigpark <hikarioozono@gmail.com>
2022-07-14 16:22:07 +0900

    いろいろ入れ直したりしました

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,27 +1,28 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :user
-  has_one_attached :image
-  has_many :area
-  has_many :category
-  has_many :condition
-  has_many :shipping_date
-  has_many :delivery
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :user
+    has_one_attached :image
+    has_many :area
+    has_many :category
+    has_many :condition
+    has_many :shipping_date
+    has_many :delivery
+    
+    with_options presence: true do
+      validates :image
+      validates :items_name
+      validates :explain
+    end
+    
+    with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_id
+    validates :area_id
+    validates :shipping_date_id
+    end
+    
+    
+    validates :price,      presence: true, numericality: { in: 300..9999999 }, numericality: { with: /^[0-9]+$/}
+   end
 
-  with_options presence: true do
-    validates :image
-    validates :items_name
-    validates :explain
-  end
-
-  with_options numericality: { other_than: 1 , message: "can't be blank"} do
-  validates :category_id
-  validates :condition_id
-  validates :delivery_id
-  validates :area_id
-  validates :shipping_date_id
-  end
-
-
-  validates :price,      presence: true, numericality: { in: 300..9999999 }, format: { with: /^[0-9]+$/}
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_11_060055) do
+ActiveRecord::Schema.define(version: 2022_07_14_070756) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,14 +33,19 @@ ActiveRecord::Schema.define(version: 2022_07_11_060055) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "items_name", null: false
+    t.text "explain", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "delivery_id", null: false
+    t.integer "area_id", null: false
+    t.integer "shipping_date_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -62,4 +67,5 @@ ActiveRecord::Schema.define(version: 2022_07_11_060055) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
