9b8f1cdb91c43151fc773f9c2dec94daafac9eef
LightBigpark <hikarioozono@gmail.com>
2022-07-15 11:30:37 +0900

    項目を追加しました

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -70,6 +70,11 @@ RSpec.describe Item, type: :model do
     it '全ての項目が入力されていれば出品できる' do
       expect(@item).to be_valid
     end
+    it 'userが紐付いていないと保存できない' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include('User must exist')
+    end
 
   end
 end
