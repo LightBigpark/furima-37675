7df0a0600ffdcf23501b1f116c43464d4e8d7461
LightBigpark <hikarioozono@gmail.com>
2022-07-15 11:05:18 +0900

    コンソールを削除しました

--- app/javascript/packs/item_price.js
+++ app/javascript/packs/item_price.js
@@ -7,8 +7,6 @@ window.addEventListener('load', () => {
     const profitNumber = document.getElementById("profit");
     profitNumber.innerHTML = (Math.floor(inputValue - addTaxDom));
     const value_result = inputValue * 0.1
-    console.log(value_result)
     profitNumber.innerHTML = (Math.floor(inputValue - value_result));
-      console.log(profitNumber)
   })
 })
