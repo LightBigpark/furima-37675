0eb92e4e3dd9fac60e90ef840ccf3492e9280c26
LightBigpark <hikarioozono@gmail.com>
2022-07-18 17:06:22 +0900

    記述を一つにまとめました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, only: [:edit, :update]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -23,14 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to @item
     else
@@ -48,5 +46,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
 
 end
