025b7d8117fc9a641631d8e7d3402efd3b4e8316
LightBigpark <hikarioozono@gmail.com>
2022-07-29 17:15:32 +0900

    バリデーション

--- app/models/useraddress.rb
+++ app/models/useraddress.rb
@@ -1,3 +1,11 @@
 class Useraddress < ApplicationRecord
   belongs_to :order
+  with_options presence: true
+  validates :post_code
+  validates :area_id
+  validates :city
+  validates :addresses
+  validates :building
+  validates :phone_number
+end
 end
