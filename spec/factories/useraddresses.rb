0fc78b1c6b87a14d9b71843359dbc9ff3fd934f9
LightBigpark <hikarioozono@gmail.com>
2022-07-29 20:15:30 +0900

    Update useraddresses.rb

--- spec/factories/useraddresses.rb
+++ spec/factories/useraddresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :useraddress do
-    
+    association :order
+    post_code      {'123-4567'}
+    area_id        {'2'}
+    city           {'あああああ'}
+    addresses      {'あああああ'}
+    building       {'あああああ'}
+    phone_number   {'09012345678'}
   end
 end
