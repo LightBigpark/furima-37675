009e5d923043a0c8059f1099f6d1dc69f0108439
LightBigpark <hikarioozono@gmail.com>
2022-07-30 17:15:37 +0900

    Update useraddress.rb

--- app/models/useraddress.rb
+++ app/models/useraddress.rb
@@ -1,5 +1,7 @@
 class Useraddress < ApplicationRecord
   belongs_to :order
+  include ActiveModel::Model
+  attr_accessor :post_code, :area_id, :addresses, :building, :phone_number
   with_options presence: true
   validates :post_code format:  { with: ^\d{3}-\d{4}$}
   validates :area_id
@@ -7,4 +9,6 @@ class Useraddress < ApplicationRecord
   validates :building
   validates :phone_number format: { with: ^0\d{10,11}$}
 end
+ def save
+  
 end
