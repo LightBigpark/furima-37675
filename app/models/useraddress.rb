894531e83415651e2fcc35a30d1ca2fcc2cd916b
LightBigpark <hikarioozono@gmail.com>
2022-07-30 19:13:37 +0900

    Update useraddress.rb

--- app/models/useraddress.rb
+++ app/models/useraddress.rb
@@ -1,14 +1,15 @@
-class Useraddress < ApplicationRecord
-  belongs_to :order
+class UserAddress < ApplicationRecord
   include ActiveModel::Model
-  attr_accessor :post_code, :area_id, :addresses, :building, :phone_number
+  attr_accessor :post_code, :area_id, :addresses, :building, :phone_number, :price, :user_id
   with_options presence: true
-  validates :post_code format:  { with: ^\d{3}-\d{4}$}
+  validates :post_code format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :area_id
   validates :addresses
   validates :building
   validates :phone_number format: { with: ^0\d{10,11}$}
+  validates :price,  numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+  validates :user_id
 end
  def save
-  
+
 end
