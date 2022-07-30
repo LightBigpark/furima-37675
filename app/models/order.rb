5a1f832a80baab91ba47de60f0dde6aca66efd05
LightBigpark <hikarioozono@gmail.com>
2022-07-30 15:57:07 +0900

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :useraddress
-  include ActiveModel::Model
   attr_accessor :token
+  validates :token, presence: true
 end
