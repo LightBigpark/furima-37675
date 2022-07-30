920505ad4f9b820bd7056524fa502f528e5b15c2
LightBigpark <hikarioozono@gmail.com>
2022-07-30 22:31:11 +0900

    コントローラーのorderの部分を修正しました

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,15 @@
 class OrdersController < ApplicationController
   def index
-    @order = Order.new
+    @orderaddress = OrderAddress.new
     @item = Item.where(params[:item_id])
   end
 
   def create
-    binding.pry
-    @order = Order.new(order_params)
-    if @order.valid?
+    @orderaddress = OrderAddress.new(order_params)
+    @item = Item.where(params[:item_id])
+    if @orderaddress.valid?
       pay_item
-      @order.save
+      @orderaddress.save
       return redirect_to root_path
     else
       render 'index'
@@ -19,15 +19,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:price).merge(token: params[:token])
+    params.require(:orderaddress).permit(:post_code, :area_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
     )
   end
+
 end
