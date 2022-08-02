070f81f75216381eb5d955a4f22cc3668d3c19e6
LightBigpark <hikarioozono@gmail.com>
2022-08-02 15:14:33 +0900

    オプションを削除しました

--- db/migrate/20220730095436_create_addresses.rb
+++ db/migrate/20220730095436_create_addresses.rb
@@ -6,7 +6,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.integer    :area_id,      null: false
       t.string     :city,         null: false
       t.string     :addresses,    null: false
-      t.string     :building,     null: false
+      t.string     :building
       t.string     :phone_number, null: false
       t.timestamps
     end
